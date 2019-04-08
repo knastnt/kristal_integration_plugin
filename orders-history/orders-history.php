93339e90cd26b2060d243b4bf07ac7904a8916a8
knastnt <456kot@mail.ru>
2019-04-08 20:09:34 +1000

    перебор ids

--- orders-history/orders-history.php
+++ orders-history/orders-history.php
@@ -5,6 +5,15 @@
 //Заказы берутся из куков браузера. Куки пишет javascript в файле wp-content/plugins/kristal_integration/checkout/checkout_tuning.php
 
 function orders_cookies_history_func(){
-    return "foo = ";
+    if( isset( $_COOKIE['previousorders'])) {
+        $orderIds = explode(',', $_COOKIE['previousorders']);
+
+        foreach ($orderIds as $orderId) {
+            echo $orderId;
+        }
+
+    }else{
+        echo "Нет сохраненной истории заказов.";
+    }
 }
 add_shortcode('orders_cookies_history', 'orders_cookies_history_func');
