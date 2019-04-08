df94951779f3be739cf68ad3524f53523fb44d2c
knastnt <456kot@mail.ru>
2019-04-08 22:44:49 +1000

    разбил по div ам

--- orders-history/orders-history.php
+++ orders-history/orders-history.php
@@ -5,6 +5,7 @@
 //Заказы берутся из куков браузера. Куки пишет javascript в файле wp-content/plugins/kristal_integration/checkout/checkout_tuning.php
 
 function orders_cookies_history_func(){
+    echo '<div class="orders_cookies_history">';
     if( isset( $_COOKIE['previousorders'])) {
         $orderIds = explode(',', $_COOKIE['previousorders']);
 
@@ -15,20 +16,36 @@ function orders_cookies_history_func(){
 
             if ($order == false) continue;
 
-            echo $order->get_id() . "<br>";
-            echo $order->get_date_created() . "<br>";
-            echo $order->get_total() . " " . $order->get_currency() . "<br>";
+            ?>
 
-            $items = $order->get_items();
-            foreach ($items as $item){
-                //var_dump($item);
-                echo $item->get_name() . "<br>";
-                echo $item->get_data()['total'] . "<br>";
-            }
+            <div class="single_order">
+                <div class="header">
+                    <div class="orderid">Заказ № <?php echo $order->get_id(); ?> </div>
+                    <div class="orderdate"><?php echo $order->get_date_created(); ?> </div>
+                    <div class="ordertotal"><?php echo $order->get_total() . " " . $order->get_currency(); ?> </div>
+                </div>
+                <div class="details">
+                    <?php
+                        $items = $order->get_items();
+                        foreach ($items as $item) {
+                            ?>
+                            <div class="item">
+                                <div class="orderdate"><?php echo $item->get_name(); ?> </div>
+                                <div class="ordertotal"><?php echo $item->get_data()['total']; ?> </div>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                </div>
+
+            </div>
+
+            <?php
         }
 
     }else{
         echo "Нет сохраненной истории заказов.";
     }
+    echo '</div>';
 }
 add_shortcode('orders_cookies_history', 'orders_cookies_history_func');
