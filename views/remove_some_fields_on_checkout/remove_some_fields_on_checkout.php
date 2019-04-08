ad31b659438c7a45bd87683bd39008aa3d74c7f8
knastnt <456kot@mail.ru>
2019-04-08 14:26:07 +1000

    убрал E-Mail из чекаута

--- views/remove_some_fields_on_checkout/remove_some_fields_on_checkout.php
+++ views/remove_some_fields_on_checkout/remove_some_fields_on_checkout.php
@@ -18,13 +18,13 @@ function custom_override_checkout_fields( $fields ) {
 	unset($fields['billing']['billing_state']); //штат
 	//unset($fields['billing']['billing_phone']); //телефон
 	unset($fields['order']['order_comments']); //добавить комментарий
-	//unset($fields['billing']['billing_email']); //email
+	unset($fields['billing']['billing_email']); //email
 	//unset($fields['account']['account_username']); //логин
 	//unset($fields[‘account’][‘account_password’]); //пароль
 	//unset($fields['account']['account_password-2']); //подтверждение пароля
 	
 	//Не обязателен E-mail если человек не залогинен
-	$fields['billing']['billing_email']['required'] = false;
+	//$fields['billing']['billing_email']['required'] = false;
 	?>
 		<style>
 			#billing_country_field {
