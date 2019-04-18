a15f688b54f9627d3cde4b9347f373aac9ca898d
knastnt <456kot@mail.ru>
2019-04-18 12:51:31 +1000

    is_admin() в шорткод orders_cookies_history

--- orders-history/orders-history.php
+++ orders-history/orders-history.php
@@ -5,29 +5,37 @@
 //Заказы берутся из куков браузера. Куки пишет javascript в файле wp-content/plugins/kristal_integration/checkout/checkout_tuning.php
 
 function orders_cookies_history_func(){
-    echo '<div class="orders_cookies_history">';
-    if( isset( $_COOKIE['previousorders'])) {
-        $orderIds = explode(',', $_COOKIE['previousorders']);
+    $isadmin = is_admin();
+    if (!$isadmin) {
 
-        $orderIds = array_reverse($orderIds);
+        echo '<div class="orders_cookies_history">';
+        if (isset($_COOKIE['previousorders'])) {
+            $orderIds = explode(',', $_COOKIE['previousorders']);
 
-        foreach ($orderIds as $orderId) {
+            $orderIds = array_reverse($orderIds);
 
-            // Get an instance of the WC_Order object (same as before)
-            $order = wc_get_order( $orderId );
+            $is_empty = true;
 
-            if ($order == false) continue;
+            foreach ($orderIds as $orderId) {
 
-            ?>
+                // Get an instance of the WC_Order object (same as before)
+                $order = wc_get_order($orderId);
 
-            <div class="single_order">
-                <div class="orders_cookies_history_header">
-                    <div class="orderid">Заказ № <?php echo $order->get_id(); ?> </div>
-                    <div class="orderdate">от <?php echo $order->get_date_created()->date('d.m.Y g:i'); ?> </div>
-                    <div class="ordertotal">На сумму: <?php echo $order->get_total() . " " . $order->get_currency(); ?> </div>
-                </div>
-                <div class="details">
-                    <?php
+                if ($order == false) continue;
+
+                $is_empty = false;
+
+                ?>
+
+                <div class="single_order">
+                    <div class="orders_cookies_history_header">
+                        <div class="orderid">Заказ № <?php echo $order->get_id(); ?> </div>
+                        <div class="orderdate">от <?php echo $order->get_date_created()->date('d.m.Y g:i'); ?> </div>
+                        <div class="ordertotal">На
+                            сумму: <?php echo $order->get_total() . " " . $order->get_currency(); ?> </div>
+                    </div>
+                    <div class="details">
+                        <?php
                         $items = $order->get_items();
                         foreach ($items as $item) {
                             //var_dump($item->get_data());
@@ -39,40 +47,42 @@ function orders_cookies_history_func(){
                             <?php
                         }
                         ?>
+                    </div>
+
                 </div>
 
-            </div>
 
-
-
-
-
-            <?php
-        }
-
-        ?>
-        <script>
-            var acc = document.getElementsByClassName("orders_cookies_history_header");
-            var i;
-
-            for (i = 0; i < acc.length; i++) {
-                acc[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
-                    var panel = this.nextElementSibling;
-                    if (panel.style.maxHeight){
-                        panel.style.maxHeight = null;
-                    } else {
-                        panel.style.maxHeight = panel.scrollHeight + "px";
-                    }
-                });
+                <?php
             }
-        </script>
-        <?php
 
-    }else{
-        echo "Нет сохраненной истории заказов.";
+            if ($is_empty) {
+                echo "Нет сохраненной истории заказов.";
+            }
+
+            ?>
+            <script>
+                var acc = document.getElementsByClassName("orders_cookies_history_header");
+                var i;
+
+                for (i = 0; i < acc.length; i++) {
+                    acc[i].addEventListener("click", function () {
+                        this.classList.toggle("active");
+                        var panel = this.nextElementSibling;
+                        if (panel.style.maxHeight) {
+                            panel.style.maxHeight = null;
+                        } else {
+                            panel.style.maxHeight = panel.scrollHeight + "px";
+                        }
+                    });
+                }
+            </script>
+            <?php
+
+        } else {
+            echo "Нет сохраненной истории заказов.";
+        }
+        echo '</div>';
     }
-    echo '</div>';
 }
 add_shortcode('orders_cookies_history', 'orders_cookies_history_func');
 
