4ed17db5e8f418c831c70c12fabc29e52203b843
knastnt <456kot@mail.ru>
2019-07-22 15:25:17 +1000

    ajax запрос

--- shortcodes/getter-blocks-content.php
+++ shortcodes/getter-blocks-content.php
@@ -3,8 +3,7 @@
 //Шорткоды заполнения блоков Торговая площадка, Вебинары, Частные объявления
 
 function get_kristal_trade_place_func(){
-    $kristall_options_array = get_option('kristall_options_array');
-    $apiLink = isset($kristall_options_array['kristall_api_url']) ? $kristall_options_array['kristall_api_url'] : 'https://www.kristal-online.ru/api/api.php';
+
 
 }
 add_shortcode('kristal_trade_place', 'get_kristal_trade_place_func');
@@ -17,8 +16,36 @@ function get_kristal_vebinars_func(){
 add_shortcode('kristal_vebinars', 'get_kristal_vebinars_func');
 
 
-function get_kristal_private_ads_func(){
+function get_kristal_private_ads_func( $atts ){
+    $kristall_options_array = get_option('kristall_options_array');
+    $apiLink = isset($kristall_options_array['kristall_api_url']) ? $kristall_options_array['kristall_api_url'] : 'https://www.kristal-online.ru/api/api.php';
 
+
+    $apiLink .= '?data=getObData';
+
+    if (isset($atts['count']) && intval($atts['count']) > 0) {
+        $apiLink .= '&count=' . intval($atts['count']);
+    }
+    if (isset($atts['type']) && strtolower($atts['type']) == 'all') {
+        $apiLink .= '&type=all';
+    }
+
+    ?>
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+
+            // Запустим ajax-запрос, установим обработчики его выполнения и
+            // сохраним объект jqxhr данного запроса для дальнейшего использования.
+            var jqxhr = jQuery.getJSON("<?php echo $apiLink; ?>")
+                .success(function() { console.log("Успешное выполнение"); })
+                .error(function() { console.log("Ошибка выполнения. Возможно, Ваш запрос кроссдоменный - см. CORS и Access-Control-Allow-Origin"); })
+                .complete(function() { /*alert("Завершение выполнения");*/ });
+
+
+
+        });
+    </script>
+    <?php
 
 }
 add_shortcode('kristal_private_ads', 'get_kristal_private_ads_func');
